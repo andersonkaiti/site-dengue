bbeb71663e4093dbff9ad207628bea7bbd19ebef
andersonkaiti <anderkaiti@gmail.com>
2024-05-11 09:09:32 -0300

    Corrigindo padding presente na tag main que contém o footer

--- src/app/noticias/page.tsx
+++ src/app/noticias/page.tsx
@@ -2,7 +2,7 @@ import Footer from "@/components/footer/footer";
 
 export default function Noticia () {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-screen flex-col items-center justify-between pt-24">
             <h1>Notícias</h1>
             <Footer/>
         </main>
--- src/app/postos-de-saude/page.tsx
+++ src/app/postos-de-saude/page.tsx
@@ -2,7 +2,7 @@ import Footer from "@/components/footer/footer";
 
 export default function PostosDeSaude() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-screen flex-col items-center justify-between pt-24">
             <h1>Postos de Saúde</h1>
             <Footer/>
         </main>
