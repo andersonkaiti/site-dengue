47fa486e3c654c23d38665abb24a1ebf3cdfea09
andersonkaiti <anderkaiti@gmail.com>
2024-05-26 03:15:52 -0300

    fix: Corrigindo erro de responsividade nas imagens da tela de Sintomas

--- src/app/noticias/page.tsx
+++ src/app/noticias/page.tsx
@@ -41,9 +41,7 @@ export default function Noticias() {
                     </div>
                 </section>
                 <section className={styles.section}>
-                    <div className="title-one">
-                        <h1>&quot;Alerta Dengue: Mantenha-se Informado, <span> Proteja-se e Faça a Diferença!</span>&quot;</h1>
-                    </div>
+                    <h1 className="title-one">&quot;Alerta Dengue: Mantenha-se Informado, <span> Proteja-se e Faça a Diferença!</span>&quot;</h1>
                     <div className={styles.gallery}>
                         <div className={styles.galleryImage}>
                             <Image
@@ -62,17 +60,13 @@ export default function Noticias() {
                     </div>
                 </section>
                 <section className={styles.section}>
-                    <div className="title-one">
-                        <h1>Eventos e Iniciativas</h1>
-                    </div>
+                    <h1 className="title-one">Eventos e Iniciativas</h1>
                     <div className={styles.text}>
                         Para combater a dengue, diversas iniciativas e eventos são realizados, como campanhas de conscientização sobre a eliminação de criadouros do mosquito Aedes aegypti, mutirões de limpeza para remover recipientes com água parada, e o uso de fumacê para reduzir a população de mosquitos. Programas de vigilância monitoram áreas de risco, enquanto inovações tecnológicas, como mosquitos geneticamente modificados e aplicativos de denúncia, auxiliam no controle. Eventos como o Dia Nacional de Combate ao Aedes aegypti mobilizam a sociedade, destacando a importância da participação coletiva e da responsabilidade individual na prevenção da doença.
                     </div>
                 </section>
                 <section className={styles.section}>
-                    <div className="title-two">
-                        <h1>Alertas e Informações sobre a Dengue</h1>
-                    </div>
+                    <h1 className="title-one">Alertas e Informações sobre a Dengue</h1>
                     <div className={styles.gallery}>
                         <div className={styles.galleryImage}>
                             <Image
@@ -105,17 +99,13 @@ export default function Noticias() {
                     </div>
                 </section>
                 <section className={styles.section}>
-                    <div className="title-one">
-                        <h1>Campanhas de conscientização</h1>
-                    </div>
+                    <h1 className="title-one">Campanhas de conscientização</h1>
                     <div className={styles.text}>
                         Diversas campanhas de conscientização têm sido realizadas para combater a dengue, como a campanha “10 Minutos Contra a Dengue” do Ministério da Saúde do Brasil, que incentiva a população a dedicar 10 minutos por semana para eliminar focos do mosquito em suas casas, e a campanha &quot;Todos Contra o Mosquito&quot;, que utiliza mídias sociais, televisão e rádio para educar sobre a prevenção de dengue, zika e chikungunya. A &quot;Semana Nacional de Mobilização contra o Aedes aegypti&quot; envolve ações em escolas e comunidades para conscientizar sobre os cuidados necessários, enquanto a campanha &quot;Dengue: Aqui Mosquito Não se Cria&quot;, da Fiocruz, distribui materiais educativos e realiza palestras e oficinas. A campanha &quot;Xô Dengue&quot;, promovida por prefeituras como a de São Paulo, inclui a distribuição de folhetos, visitas domiciliares por agentes de saúde e o uso de carros de som para alertar sobre a importância de eliminar criadouros. Essas iniciativas são essenciais para engajar a comunidade e promover práticas simples de prevenção da dengue.
                     </div>
                 </section>
                 <section className={styles.section}>
-                    <div className="title-one">
-                        <h1>Novidade de pesquisa</h1>
-                    </div>
+                    <h1 className="title-one">Novidade de pesquisa</h1>
                     <div className={styles.text}>
                         Recentes avanços na pesquisa sobre a dengue incluem o desenvolvimento de uma vacina mais eficaz e segura, com a Qdenga (TAK-003), da Takeda, mostrando resultados promissores em testes clínicos. Além disso, a implementação de mosquitos Aedes aegypti geneticamente modificados e infectados com a bactéria Wolbachia tem demonstrado sucesso em reduzir significativamente a transmissão do vírus. Pesquisas também estão explorando novos métodos de diagnóstico rápido e preciso, utilizando tecnologias como CRISPR para identificar a presença do vírus em estágios iniciais. Esses avanços prometem melhorar significativamente o controle e a prevenção da dengue em áreas endêmicas..
                     </div>
