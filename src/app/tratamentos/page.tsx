84d1739ca659074cf04c1319639589ee823e77c1
l3and49 <126525698+l3and49@users.noreply.github.com>
2024-06-14 20:59:34 -0300

    Inclusão dos textos e imagens na tela de tratamentos

--- src/app/tratamentos/page.tsx
+++ src/app/tratamentos/page.tsx
@@ -3,47 +3,132 @@ import styles from "./tratamentos.module.css";
 import Image from "next/image";
 
 const imageBorder = { borderRadius: '10px' };
-const width = 400;
-const height = 400;
 
 export default function Tratamentos() {
     return (
-        <main>
-            <div className={styles.TituloPrincipal}>
-                <h1>Conheça os Tratamentos e Proteja Sua Saúde!</h1>
-            </div>
-            <div className={styles.container}>
-                <div className={styles.Introducao}>
-                    Espaço para introdução das imagens
+        <>
+            <main>
+                <div className={styles.TituloPrincipal}>
+                    <h1>Conheça os Tratamentos e Proteja Sua Saúde!</h1>
                 </div>
-                <div className={styles.SubCabecalho}>
-                    <h2>Tratamentos</h2>
-                    <p>Texto para tratamentos</p>
-                </div>
-                <div className={styles.SubCabecalho}>
-                    <h2>Identificação dos sintomas</h2>
-                    <p>Texto para Identificação dos sintomas</p>
-                    <p>Imagem do mosquito</p>
-                </div>
-                <div className={styles.SubCabecalho}>
-                    <h2>Tratamentos de casos graves</h2>
-                    <p>Texto para Tratamentos de casos graves</p>
-                    <p>Imagem do mosquito</p>
-                </div>  
-                <div className={styles.SubCabecalho}>
-                    <h2>Medicações Indicadas e Contraindicadas</h2>
-                    <p>Texto para Medicações Indicadas e Contraindicadas</p>
-                    <p>Imagem do mosquito</p>
-                    <p>Imagens do senhor Idoso</p>
-                </div>  
-                <div className={styles.SubCabecalho}>
-                    <h2>Recuperação e Cuidados Pós-Tratamento</h2>
-                    <p>Texto para Recuperação e Cuidados Pós-Tratamento</p>
-                    <p>Imagem do mosquito</p>
-                    <p>Imagens do senhor Idoso</p>
-                </div>  
-            </div> 
-            <Footer/>
-        </main>
+                <div className={styles.container}>
+                    <div className={styles.Introducao}>
+                        Espaço para introdução das imagens
+                    </div>
+                    <div className={styles.SubCabecalho}>
+                        <h2>Tratamentos</h2>
+                        <p>
+                            A dengue é uma doença séria que requer atenção imediata. Iniciar o tratamento precocemente é crucial para minimizar complicações e garantir uma recuperação mais rápida. Nosso objetivo é fornecer informações claras e detalhadas para ajudar você a entender os diferentes aspectos do tratamento da dengue.
+                            <br></br>
+                            <br></br>
+                            No nosso site, você encontrará orientações abrangentes sobre como identificar os sintomas, quais medidas tomar em casos leves, moderados e graves, e quais medicamentos são recomendados ou contraindicados. Além disso, oferecemos dicas sobre cuidados complementares para apoiar a recuperação, prevenir complicações e responder às perguntas mais comuns sobre a doença.
+                            Estamos comprometidos em ser uma fonte confiável de informação, colaborando com profissionais de saúde e usando tecnologia para disponibilizar recursos úteis e acessíveis para toda a comunidade. Juntos, podemos combater a dengue de maneira mais eficaz e proteger a saúde de todos.
+                        </p>
+                    </div>
+                    <div className={styles.SubCabecalho}>
+                        <h2>Identificação dos sintomas</h2>
+                        <div className={styles.FlexContainer}>
+                            <p>
+                                Reconhecer os sintomas da dengue é o primeiro passo para buscar tratamento. Os sintomas iniciais incluem febre alta, dores de cabeça, dores atrás dos olhos, nas articulações e nos músculos. Fique atento a sinais de alerta como sangramentos, dor abdominal intensa e vômitos persistentes.
+                                <br></br>
+                                <br></br>
+                                A identificação precoce desses sintomas permite uma intervenção rápida e eficaz, reduzindo o risco de complicações graves. Além disso, é importante diferenciar os sintomas da dengue de outras doenças febris, para garantir o tratamento adequado. Caso observe qualquer um dos sinais de alerta, procure imediatamente um serviço de saúde. O acompanhamento médico é essencial para monitorar a evolução da doença e ajustar o tratamento conforme necessário.
+                                <br></br>
+                                <br></br>
+                                Lembre-se de que a dengue pode evoluir rapidamente, por isso, a vigilância constante dos sintomas e a busca por orientação profissional são fundamentais para uma recuperação segura.
+                            </p>
+                            <div className={styles.ImagemMosquito}>
+                                <Image
+                                    src="/tratamentos/MosquitoTratamentos.jpg"
+                                    layout="fill"
+                                    objectFit="cover"
+                                    alt="Mosquito"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.SubCabecalho}>
+                        <h2>Tratamentos de casos leves</h2>
+                        <p>
+                            Para casos leves de dengue, o tratamento é baseado em repouso, hidratação oral e uso de medicamentos para aliviar a febre e as dores. É essencial evitar anti-inflamatórios, como o ibuprofeno, que podem agravar o quadro.
+                            <br></br>
+                            <br></br>
+                            A ingestão de líquidos, como água, sucos naturais e soluções de reidratação oral, é crucial para prevenir a desidratação, que é uma complicação comum da dengue. O paracetamol é recomendado para o controle da febre e das dores, devido ao seu perfil de segurança mais adequado para pacientes com dengue.
+                            <br></br>
+                            <br></br> 
+                            Além disso, é importante manter uma alimentação leve e nutritiva, rica em frutas, legumes e alimentos de fácil digestão, para ajudar na recuperação do organismo. Evite alimentos gordurosos ou pesados, que podem sobrecarregar o sistema digestivo.
+                            O acompanhamento regular de um profissional de saúde é fundamental, mesmo em casos leves, para monitorar o progresso da doença e ajustar as orientações conforme necessário. Fique atento a qualquer mudança nos sintomas e, em caso de piora, procure assistência médica imediatamente.
+                        </p>
+                    </div>  
+                    <div className={styles.SubCabecalho}>
+                        <h2>
+                            Tratamentos de casos graves
+                            <br></br>
+                            (Dengue Hemorrágica)
+                        </h2>
+                        <p>
+                            Para casos graves de dengue, a hospitalização é indispensável. Os pacientes recebem cuidados intensivos, incluindo reposição de líquidos intravenosos e, em situações extremas, transfusões de sangue.
+                            <br></br>
+                            <br></br>
+                            A monitorização constante dos sinais vitais é crucial para a sobrevivência, garantindo o manejo de complicações como desidratação, sangramentos graves e falência de órgãos. Equipes médicas especializadas cuidam de intervenções críticas e suporte nutricional, adaptando o tratamento às necessidades individuais de cada paciente. A cooperação e comunicação entre profissionais de saúde e familiares são essenciais para a recuperação do paciente.
+                        </p>
+                    </div>
+                    <div className={styles.SubCabecalho}>
+                        <h2>
+                            Medicações Indicadas e Contraindicadas
+                        </h2>
+                        <p>
+                            O uso de medicamentos deve ser cuidadosamente monitorado. Paracetamol pode ser usado para controlar a febre e aliviar as dores, pois é seguro e eficaz no contexto da dengue. Enquanto isso, anti-inflamatórios como ibuprofeno e aspirina devem ser evitados devido ao risco de sangramentos e complicações hemorrágicas, comuns na dengue.
+                            <br></br>
+                            <br></br>
+                            É fundamental seguir as orientações médicas quanto à dosagem e frequência do paracetamol para evitar a toxicidade hepática. Além disso, qualquer outro medicamento só deve ser administrado sob supervisão médica, para garantir que não interfira no tratamento ou agrave a condição do paciente.
+                            <br></br>
+                            <br></br>
+                            Pacientes com dengue devem também evitar medicamentos combinados que possam conter anti-inflamatórios em sua composição. Em caso de dúvidas sobre o uso de qualquer medicamento, consulte um profissional de saúde para obter recomendações adequadas e seguras.
+                        </p>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <div className={styles.ImagensTratamentos}>
+                            <div className={styles.ImagemColuna}>
+                                <div className={styles.ImagemTratamento}>
+                                    <Image
+                                        src="/tratamentos/IdosoTristeTratamentos.jpg"
+                                        layout="fill"
+                                        objectFit="cover"
+                                        alt="Idoso triste"
+                                    />
+                                </div>
+                                <div className={styles.ImagemTratamento}>
+                                    <Image
+                                        src="/tratamentos/MaosDadasTratamentos.png"
+                                        layout="fill"
+                                        objectFit="cover"
+                                        alt="Idoso coberto"
+                                    />
+                                </div>
+                            </div>
+                            <div className={styles.ImagemColuna}>
+                                <div className={styles.ImagemTratamento}>
+                                    <Image
+                                        src="/tratamentos/IdosoCobertoTratamentos.jpg"
+                                        layout="fill"
+                                        objectFit="cover"
+                                        alt="Mãos dadas"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>  
+                    <div className={styles.SubCabecalho}>
+                        <h2>Recuperação e Cuidados Pós-Tratamento</h2>
+                        <p>
+                            Durante esse período, é importante continuar com a hidratação, manter uma dieta equilibrada e evitar atividades físicas intensas até a completa recuperação. A dengue pode deixar o corpo enfraquecido, portanto, é essencial fornecer os nutrientes necessários para fortalecer o sistema imunológico e acelerar o processo de recuperação. Além disso, é fundamental evitar o consumo de álcool e cigarros, pois podem prejudicar ainda mais a saúde e retardar a recuperação. Se os sintomas persistirem ou piorarem durante o período de recuperação, é importante buscar orientação médica para garantir que não haja complicações adicionais.
+                        </p>
+                    </div>  
+                </div> 
+                <Footer/>
+            </main>
+        </>
     );
 }
